fa3506adec8bd50580878d76a293e3d611942474
Joseph Daniel Chapdelaine <orvist@gmail.com>
2016-09-21 10:24:14 -0500

    modularized routes

--- database.js
+++ database.js
@@ -2,12 +2,12 @@
 // mongoose allows you to use the data base all over the place with out passing the db obj around
 const mongoose = require('mongoose');
 //connect to mongo locally
-// const MONGODB_URL = 'mongodb://localhost:27017/pizzadeath';
+const MONGODB_URL = 'mongodb://localhost:27017/pizzadeath';
 //push to robmon locally
 // const MONGODB_URL = mlab url with username and password
 //this is set on heroku under setting/Config Vars
 
-const MONGODB_URL = process.env.MONGODB_URL || 'mongodb://localhost:27017/pizzadeath'
+// const MONGODB_URL = process.env.MONGODB_URL || 'mongodb://localhost:27017/pizzadeath'
 /////////////////////////////////////////
 // Tell mongoose to use regular promise
 mongoose.Promise = Promise
